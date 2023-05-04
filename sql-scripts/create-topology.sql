641b71a654430e4b64be9b22b3cb0c2960fb4cf1
048118247 <lamberto.pauletti@movyon.com>
2023-05-04 22:28:40 +0200

    updated queries

--- sql-scripts/create-topology.sql
+++ sql-scripts/create-topology.sql
@@ -1,21 +1,21 @@
-SELECT  * INTO vertices_table from pgr_extractVertices('SELECT gid as id, the_geom as geom FROM ways');
+SELECT  * INTO vertices from pgr_extractVertices('SELECT gid as id, the_geom as geom FROM ways');
 
 
 WITH
 out_going AS (
-  SELECT id AS vid, unnest(out_edges) AS eid, x, y
-  FROM vertices_table
+  SELECT id AS vid, unnest(out_edges) AS eid
+  FROM vertices
 )
 UPDATE ways
-SET source = vid, x1 = x, y1 = y
+SET source = vid
 FROM out_going WHERE gid = eid;
 
 
 WITH
 in_coming AS (
-  SELECT id AS vid, unnest(in_edges) AS eid, x, y
-  FROM vertices_table
+  SELECT id AS vid, unnest(in_edges) AS eid
+  FROM vertices
 )
 UPDATE ways
-SET target = vid, x2 = x, y2 = y
+SET target = vid
 FROM in_coming WHERE gid = eid;
--- ways-graph-queries.sql
+++ ways-graph-queries.sql
@@ -12,7 +12,7 @@ order by c.component;
 
 -- invoke the pgr_dijkstra pgrouting function to find the shortest path between 2 nodes
 SELECT * FROM pgr_dijkstra(
-    'SELECT gid as id, source, target, length as cost, length as reverse_cost FROM ways',
+    'SELECT gid as id, source, target, cost, cost as reverse_cost FROM ways',
     <node-origin>, <node-destination>
 );
 
@@ -21,32 +21,20 @@ select  w.name, w.source, w.target, d.cost, d.agg_cost FROM ways as w right join
 pgr_dijkstra(
     'SELECT gid as id, source, target, cost FROM ways',
     <node-origin>, <node-destination>
-) as d on d.edge = w.gid ;
-
-
--- create a route_analysis table, used for paths storing
-CREATE TABLE route_analysis
-(
-  id bigserial NOT NULL,
-  from_node integer NOT NULL,
-  to_node integer NOT NULL,
-  length double precision,
-  the_geom geometry(LineString,4326),
-  CONSTRAINT routing_pkey PRIMARY KEY (id)
-);
+) as d on d.edge = w.gid;
 
 -- insert from_node to_node and dijksta total cost of the path 
 insert into route_analysis (from_node, to_node, length)
-SELECT distinct  <node-origin>, <node-destination>, agg_cost FROM pgr_dijkstra(
+SELECT distinct <node-origin>, <node-destination>, agg_cost FROM pgr_dijkstra(
     'SELECT gid as id, source, target, cost, cost as reverse_cost FROM ways',
     <node-origin>, <node-destination>
-) where edge = -1 ;
+) where edge = -1;
 
 -- insert the geometry of the path calculated between of from_node and to_node
 update route_analysis r 
 set (the_geom) = (select st_makeline(pgr.the_geom)
 from (SELECT w.the_geom  FROM pgr_dijkstra(
-    'SELECT gid as id, source, target,cost,  cost as reverse_cost FROM ways',
+    'SELECT gid as id, source, target, cost, cost as reverse_cost FROM ways',
      <node-origin>, <node-destination>
 ) as d left join ways as w on d.edge = w.gid) as pgr)
 where r.from_node =  <node-origin> and r.to_node = <node-destination>;
